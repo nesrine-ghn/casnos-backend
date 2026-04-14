7d21a9481f76126ab3dd6c19b18bd3ac795af425
nesrine <nesrineghenaiar@gmail.com>
2026-04-14 01:38:29 +0100

    Configure backend for deployment

--- config/db.js
+++ config/db.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST ,
-  user: process.env.DB_USER , 
-  password: process.env.DB_PASSWORD ,
-  database: process.env.DB_NAME 
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // 👈 ADD THIS
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
