1d900005f8efc33b4613aee911cdd2528db7a3d8
nesrine <nesrineghenaiar@gmail.com>
2026-04-14 18:40:34 +0100

    Fix database connection and CORS for production

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST ,
   port: process.env.DB_PORT, // 👈 ADD THIS
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
